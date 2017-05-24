5b0fbda18b6a2d601cd20cd8eb8475794c55319c
Tolu Oyeniyi <tjoye20@gmail.com>
2017-05-24 00:59:21 -0500

    Add calendar scope to google oauth.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   # provider :twitter, ENV["CONSUMER_KEY"], ENV["CONSUMER_SECRET"]
   provider :facebook, ENV["FB_APP_ID"], ENV["FB_APP_SECRET"],
   scope: "user_events"
-  provider :google_oauth2, ENV["GOOGLE_API_ID"], ENV["GOOGLE_API_SECRET"]
+  provider :google_oauth2, ENV["GOOGLE_API_ID"], ENV["GOOGLE_API_SECRET"],
+  scope: "calendar"
 end
