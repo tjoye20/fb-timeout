7988c9b0c1ad3f97534717500363d081f665b31c
Tolu Oyeniyi <tjoye20@gmail.com>
2017-06-03 13:13:27 -0500

    Scope event creation to current_user.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 class EventsController < ApplicationController
 	before_action :user_signed_in?
-	
+
 	def index 
 		@events = current_user.events
 	end 
@@ -8,7 +8,7 @@ class EventsController < ApplicationController
 	def create_events 
 		fb_client.get_object("me/events/not_replied").each do |event|
 			if !Event.find_by(event_id: event["id"]) && event["start_time"] > Time.now
-				Event.create(
+				current_user.events.create(
 					name: event["name"],
 					info: event["description"],
 					date: event["start_time"],
