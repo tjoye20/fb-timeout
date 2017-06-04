2219587d4f9d7e125627796440e1b2d38ff2d7a8
Tolu Oyeniyi <tjoye20@gmail.com>
2017-06-04 02:11:54 -0500

    Add default from to mailer. Update fb oauth permissions.

--- app/mailers/events_mailer.rb
+++ app/mailers/events_mailer.rb
@@ -1,4 +1,6 @@
 class EventsMailer < ApplicationMailer
+	default from: 'no-reply@nosocial.com'
+
 	def new_events(name, email, event)
 		@name = name 
 		@email = email 
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   # provider :twitter, ENV["CONSUMER_KEY"], ENV["CONSUMER_SECRET"]
   provider :facebook, ENV["FB_APP_ID"], ENV["FB_APP_SECRET"],
-  scope: "user_events"
+  scope: "user_events,manage_pages,pages_show_list,rsvp_event"
   provider :google_oauth2, ENV["GOOGLE_API_ID"], ENV["GOOGLE_API_SECRET"],
   scope: "calendar"
 end
