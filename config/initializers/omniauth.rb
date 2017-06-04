b0704e8e08d02a2fad331bebc671f6a93280d066
Tolu Oyeniyi <tjoye20@gmail.com>
2017-06-04 02:23:38 -0500

    Change scope in fb oauth back.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  # provider :twitter, ENV["CONSUMER_KEY"], ENV["CONSUMER_SECRET"]
   provider :facebook, ENV["FB_APP_ID"], ENV["FB_APP_SECRET"],
-  scope: "user_events,manage_pages,pages_show_list,rsvp_event"
+  scope: "user_events"
   provider :google_oauth2, ENV["GOOGLE_API_ID"], ENV["GOOGLE_API_SECRET"],
   scope: "calendar"
 end
